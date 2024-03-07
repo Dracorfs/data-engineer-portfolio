957687d224bc37a448f0f6813e725fe83cfcdb5d
Dracorfs <franciscosantopietro@hotmail.com>
2024-03-07 18:53:14 -0300

    FIX: &apos;

--- components/Landing.js
+++ components/Landing.js
@@ -18,7 +18,7 @@ export default function Landing() {
                     <div className="lg:w-1/2 lg:order-first order-last">
                         <div className=" text-center lg:text-left">
                             <div>
-                                <p className="text-[#9ae5db] font-semibold text-2xl mb-2 leading-none">Hi, I'm</p>
+                                <p className="text-[#9ae5db] font-semibold text-2xl mb-2 leading-none">Hi, I&apos;m</p>
                                 <h1 className="text-4xl font-title font-black leading-none md:text-6xl lg:text-5xl 2xl:text-7xl text-white">{headerData.nombre}</h1>
                                 <p className="mb-4 mt-2 lg:mt-0 text-2xl font-semibold font-secundary leading-none md:text-3xl lg:text-4xl text-textdestacado"> {headerData.titulo}</p>
                                 <p className="mb-6 text-lg font-normal text-grisclaro2 md:text-base 2xl:text-xl [text-wrap:balance]">{headerData.descripcion}</p>
